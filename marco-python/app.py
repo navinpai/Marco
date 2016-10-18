d2c2e4c8c56efed82ca723a9496ffc2e2d3e4fad
Navin Pai <lifeofnavin@gmail.com>
2016-10-19 01:10:22 +0530

    Parse url text to get set of places

--- marco-python/app.py
+++ marco-python/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from geotext import GeoText
+import requests
 from configs import *
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return READABILITY_TOKEN
+@app.route('/updateProfile', methods=['GET', 'POST'])
+def update_profile():
+  if request.method == 'POST':
+    url = request.json['url']
+    r = requests.get("http://www.readability.com/api/content/v1/parser?url=" + url + "&token=" + READABILITY_TOKEN) 
+    places_list = GeoText(r.text)
+    places_set = set(places_list.cities)
+    return str(places_set)
+  else:
+    return "This API only works with POST"
