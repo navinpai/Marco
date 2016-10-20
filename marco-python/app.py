692ee4c4f272dbc80313cb22912656b86c622d3e
Navin Pai <lifeofnavin@gmail.com>
2016-10-21 03:20:59 +0530

    Add additional API endpoints to be used by Alexa Skill

--- marco-python/app.py
+++ marco-python/app.py
@@ -2,8 +2,12 @@ from flask import Flask, request, render_template
 from geotext import GeoText
 import requests
 import MySQLdb
+from google import search
+from bs4 import BeautifulSoup
+import urllib2
 import random
 import geograpy
+import twitter
 import json
 import flickrapi
 from configs import *
@@ -45,6 +49,7 @@ def update_profile():
 @app.route('/trip')
 def get_trip():
     (place, visits) = get_place_and_details_for_user()
+    mmt_url = get_booking_url(place)
 
     flickrclient = flickrapi.FlickrAPI(FLICKR_KEY, FLICKR_SECRET, format='parsed-json')
     photos = flickrclient.photos.search(tags=[place],sort="interestingness-desc", safe_search=1, content_type=1, tag_mode="all", per_page=15)
@@ -52,5 +57,46 @@ def get_trip():
     img = photos["photos"]["photo"][ind]
     bg_img = "http://farm" + str(img["farm"])+ ".staticflickr.com/"+ str(img["server"])+"/" + str(img["id"]) + "_"+ str(img["secret"]) +"_h.jpg"
     
-    return render_template('index.html', place=place, visits=visits, bg_img=bg_img)
+    return render_template('index.html', place=place, visits=visits, bg_img=bg_img, mmt_url=mmt_url)
+
+@app.route('/trip_api')
+def get_trip_api():
+    (place, visits) = get_place_and_details_for_user()
+
+    return "You seem to have an attachment for " + place + ". You've come across it about " + str(visits) + " times" 
+
+@app.route('/what_to_do_api')
+def get_stuff_to_do():
+    city = request.args.get('city')
+
+    urls = search("Tripadvisor " + city, stop=2)
+    soup = BeautifulSoup(urllib2.urlopen(urls.next()))
+
+    result = soup.find("div", {"class":"content"})
+    return result.text
+
+def get_booking_url(city):
+    # Assuming start location as Namma Bengaluru
+    urls = search("MakeMyTrip Bangalore to " + city, stop=2)
+    return urls.next()
+
+@app.route('/book_tickets')
+def book_tickets():
+    city = request.args.get('city')
+
+    url = get_booking_url(city)
+    soup = BeautifulSoup(urllib2.urlopen(url))
+
+    table = soup.find("table", {"class":"table"})
+    tds = table.find_all('td')
+    return "Lowest Price in next 7 days is Rupees " + tds[5].renderContents()[4:-1] + " on " + tds[6].renderContents() + ". Booking your travel on that flight. Bon Voyage!"
+    return result.text
+
+@app.route('/social_api')
+def twitter_api():
+    city = request.args.get('city')
+
+    api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token_key=ACCESS_TOKEN_KEY, access_token_secret=ACCESS_TOKEN_SECRET)
+    status = api.PostUpdate("I'm planning a trip to " + city + " with #Marco. Want to join me?")
+    return "success"
 
